420a3e2a3f99cb42c75a41770fc0ffe6a323bf2c
Mariano Aguero <mariano.aguero@altoros.com>
2020-06-05 09:37:56 -0300

    Add color

--- src/components/iconFail.component.tsx
+++ src/components/iconFail.component.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export const IconFail = () => {
-  return <img src="https://icongr.am/clarity/times.svg?size=24&amp;" alt="icon" />
+  return <img src="https://icongr.am/clarity/times.svg?size=24&amp;color=d43939" alt="icon" />
 }
--- src/components/iconSuccess.component.tsx
+++ src/components/iconSuccess.component.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export const IconSuccess = () => {
-  return <img src="https://icongr.am/clarity/check.svg?size=24&amp;" alt="icon" />
+  return <img src="https://icongr.am/clarity/check.svg?size=24&amp;color=14854f" alt="icon" />
 }
